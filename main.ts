d062ac3180cea19a5ec13f4317cf6b399ad30e4a
Henry Ewurum <hcewurum@gmail.com>
2021-01-14 00:13:39 -0500

    Moving code around.

--- main.ts
+++ main.ts
@@ -1,28 +1,4 @@
-class Point{
-   
-    constructor(private _x?: number, private _y?: number){
-    }
-
-    draw(){
-        //... Code for drawing points go here
-        console.log('X: ' + this._x + ', Y: ' + this._y);
-        
-    }
-
-    get x(){
-        //... Code for getting distances go here
-        return this._x;
-    }
-
-    set x(value){
-        if(value<0)
-            throw new Error('Value cannot be less than 0.')
-
-        this._x = value;
-    }
-}
+import { Point } from './point'
 
 let point = new Point(1,2);
-let x = point.x;
-point.x = 2;
 point.draw();
